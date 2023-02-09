8509251c882dd3eed9bad02e59a9620e3ca267d4
tagpunitkr <tagpunitkumar@gmail.com>
2023-02-09 17:53:49 +0530

    minor corrections

--- lab9/prog5.c
+++ lab9/prog5.c
@@ -12,11 +12,12 @@ int main(){
     car c3={"Tata", "Tiago", 23, 4};
     car c4={"Jaguar", "Ipace", 8.6, 2};
     car c5={"Toyota", "Sresso", 11.2, 4};
+    car a[5]={c1,c2,c3,c4,c5}
     printf("Property\t\tC1\t\tC2\t\tC3\t\tC4\t\tC5\n");
-    printf("Brand\t\t\t%s \t%s \t%s \t%s\t%s\n",c1.brand,c2.brand,c3.brand,c4.brand,c5.brand);
-    printf("Model\t\t\t%s\t%s\t%s\t%s\t%s\n",c1.model,c2.model,c3.model,c4.model,c5.model);
-    printf("Milage\t\t\t%0.1f\t%0.1f\t%0.1f\t%0.1f \t%0.1f\n",c1.milage,c2.milage,c3.milage,c4.milage,c5.milage);
-    printf("Capacity\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",c1.sc,c2.sc,c3.sc,c4.sc,c5.sc);
+    printf("Brand\t\t\t%s \t%s \t%s \t%s\t%s\n",a[0].brand,a[1].brand,a[2].brand,a[3].brand,a[4].brand);
+    printf("Model\t\t\t%s\t%s\t%s\t%s\t%s\n",a[0].model,a[1].model,a[2].model,a[3].model,a[4].model);
+    printf("Milage\t\t\t%0.1f\t%0.1f\t%0.1f\t%0.1f \t%0.1f\n",a[0].milage,a[1].milage,a[2].milage,a[3].milage,a[4].milage);
+    printf("Capacity\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",a[0].sc,a[1].sc,a[2].sc,a[3].sc,a[4].sc);
     return 0;
 
 }
