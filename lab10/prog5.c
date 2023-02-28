a51295f9c8a12242b7de062ef002c134778bc11c
Punit Kumar <50498807+punitkr03@users.noreply.github.com>
2023-02-28 21:38:17 +0530

    Update prog5.c

--- lab10/prog5.c
+++ lab10/prog5.c
@@ -18,15 +18,15 @@ int main()
     }St;
 
     St s1={1, "Ayush", 8.1};
-    St s2={1, "Bull", 2.1};
-    St s3={1, "Ayan", 1.1};
-    St s4={1, "Andrew", 3.1};
-    St s5={1, "Eve", 7.7};
-    St s6={1, "Adam", 1.1};
-    St s7={1, "Apple", 8.1};
-    St s8={1, "Drew", 7.1};
-    St s9={1, "Dom", 6.1};
-    St s10={1, "Tom", 8.1};
+    St s2={2, "Bull", 2.1};
+    St s3={3, "Ayan", 1.1};
+    St s4={4, "Andrew", 3.1};
+    St s5={5, "Eve", 7.7};
+    St s6={6, "Adam", 1.1};
+    St s7={7, "Apple", 8.1};
+    St s8={8, "Drew", 7.1};
+    St s9={9, "Dom", 6.1};
+    St s10={10, "Tom", 8.1};
 
     St arr[10]={s1,s2,s3,s4,s5,s6,s7,s8,s9,s10};
 
