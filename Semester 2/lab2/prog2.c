355d0836c5f795c038f5e6edde7f263dbe21fb54
tagpunitkr <tagpunitkumar@gmail.com>
2023-04-29 20:21:43 +0530

    fix

--- Semester 2/lab2/prog2.c
+++ Semester 2/lab2/prog2.c
@@ -1,4 +1,4 @@
-//Implement a Queue using structures
+// Implement a Queue using structures
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 100
@@ -9,11 +9,13 @@ typedef struct Queue
     int rear;
 } Q;
 
-int isFull(Q *q) {
+int isFull(Q *q)
+{
     return (q->front == -1 && q->rear == -1) ? 0 : 1;
 }
 
-int isEmpty(Q *q) {
+int isEmpty(Q *q)
+{
     return (q->front == -1 && q->rear == -1) ? 1 : 0;
 }
 
@@ -22,12 +24,12 @@ void enqueue(Q *q, int item)
 
     if (!isFull(q))
     {
-        q->front=0;
-        q->rear=0;
+        q->front = 0;
+        q->rear = 0;
         q->arr[q->rear] = item;
         printf("%d is added to queue.\n", q->arr[q->rear]);
     }
-    else if (q->rear >= 9)
+    else if (q->rear >= 99)
     {
         printf("Queue Overflow!");
     }
@@ -46,7 +48,8 @@ void dequeue(Q *q)
         printf("%d is removed from queue.\n", q->arr[q->front++]);
 }
 
-void peek(Q* q) {
+void peek(Q *q)
+{
     printf("%d", q->arr[q->front]);
 }
 
@@ -64,7 +67,7 @@ int main()
     dequeue(&q);
     enqueue(&q, 5);
 
-    while (q.front<= q.rear)
+    while (q.front <= q.rear)
         dequeue(&q);
 
     return 0;
